23e3e1700e73b7db67c0abf70e03d38cf1170282
rohan-bharekar-31 <128673201+rohan-bharekar-31@users.noreply.github.com>
2025-01-16 14:20:59 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -11,31 +11,32 @@ if (navigator.geolocation) {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
-    }
-    )
+    })
 }
 
-const map=L.map("map").setView([0,0],10)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"OpenStreetMap"
-}).addTo(map)
+const map = L.map("map").setView([0, 0], 10);
 
-const marker={}
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap"
+}).addTo(map);
+
+const marker = {};
 
 socket.on("receive-Location", (data) => {
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude], 10);
+    const { id, latitude, longitude } = data;
+    // Pan to the new location without changing zoom level
+    map.panTo(new L.LatLng(latitude, longitude));
+
     if (marker[id]) {
         marker[id].setLatLng([latitude, longitude]);
     } else {
         marker[id] = L.marker([latitude, longitude]).addTo(map);
     }
-})
+});
 
 socket.on("user-disconnected", (id) => {
-    if(marker[id]){
+    if (marker[id]) {
         map.removeLayer(marker[id]);
         delete marker[id];
     }
-})  
-
+});
